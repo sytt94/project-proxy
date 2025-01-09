215f3d234f3b7fed5a0b3b753542025eafdb2584
sytt94 <86760410+sytt94@users.noreply.github.com>
2025-01-09 16:18:04 +0700

    Update function.php

--- api/function.php
+++ api/function.php
@@ -50,11 +50,11 @@ function showmik($sql_host,$sql_user,$sql_pass,$sql_db,$host,$mik_user,$mik_pass
                 echo "Error: " . $sql_delete . "<br>" . mysqli_error($connection);
             }
         }
-        
+        mysqli_close($connection); 
     } else {
         echo "Lỗi kết nối Mik: ".$host."";
     }
-    mysqli_close($connection); 
+    
 }
 
 function addlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username,$pass,$owner,$count){
@@ -67,7 +67,7 @@ function addlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username,$pass,$own
         $sql = "SELECT * FROM srcnat";
         $query = mysqli_query($connection, $sql);
         if ($query) {
-            if ($query->num_rows <= $count) {
+            if ($query->num_rows < $count) {
                 return 5; // khong đủ IP cấp
             } else {
                 while ($data = mysqli_fetch_array($query)) {
@@ -130,11 +130,86 @@ function addlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username,$pass,$own
     mysqli_close($connection); 
 }
 
-$username = '2test';
-$pass = '123456';
-$owner = 'sytt';
-$count = 3;
-//$result = showmik($sql_host,$sql_user,$sql_pass,$sql_db,$host, $mik_user, $mik_pass);
-$result = addlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username,$pass,$owner,$count);
-print_r($result);
+function exportlistuser($sql_host,$sql_user,$sql_pass,$sql_db,$username) {
+    $connection = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+    if (!$connection) {
+        return 0;
+        exit;
+    } else {
+        mysqli_set_charset($connection,"utf8");
+        $sql = "SELECT userinfo.username, radcheck.value, userinfo.notes FROM userinfo JOIN radcheck ON userinfo.username = radcheck.username WHERE userinfo.username LIKE '".$username."-%'";
+        $query = mysqli_query($connection, $sql);
+        if ($query) {
+            $result = '';
+            while ($data = mysqli_fetch_array($query)) {
+                $result = $result."\n".$data['notes'].':10012:'.$data['username'].':'.$data['value'];  
+            }
+            echo $result;
+        } else {
+            return 2; // loi sql
+        }
+        
+    }
+    mysqli_close($connection); 
+}
+
+function showuserall ($sql_host,$sql_user,$sql_pass,$sql_db,$owner){
+    $connection = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+    if (!$connection) {
+        return 0;
+        exit;
+    } else {
+        mysqli_set_charset($connection,"utf8");
+        $sql = "SELECT userinfo.username, radcheck.value, userinfo.notes, (SELECT reason FROM radpostauth WHERE radpostauth.username = userinfo.username ORDER BY authdate DESC LIMIT 1) AS lastlog , (SELECT reply FROM radpostauth WHERE radpostauth.username = userinfo.username ORDER BY authdate DESC LIMIT 1) AS reply,userinfo.creationdate, userinfo.updatedate FROM userinfo JOIN radcheck ON userinfo.username = radcheck.username WHERE userinfo.company LIKE '".$owner."'";
+        $query = mysqli_query($connection, $sql);
+        if ($query) {
+            if ($query->num_rows == 0) {
+                # chua so huu proxy-user
+                return 3; // chua so huu proxy
+            } else {
+                while ($data = mysqli_fetch_array($query)) {
+                    $date = new DateTime($data['updatedate']);
+                    $date->modify('+30 days');
+                    $expireday = $date->format('d-m-Y');
+                    $createdate = new DateTime($data['creationdate']);
+                    $createdate = $createdate->format('H:i:s d-m-Y');
+                    $result[$data['username']]['password'] = $data['value'];
+                    $result[$data['username']]['url'] = $data['notes'].':10012';
+                    if ($data['reply'] == 'Access-Accept') {
+                        $result[$data['username']]['reply'] = 1;
+                    } else {
+                        $result[$data['username']]['reply'] = 0;
+                    }
+                    $result[$data['username']]['lastlog'] = $data['lastlog'];
+                    $result[$data['username']]['creationdate'] = $createdate;
+                    $result[$data['username']]['expire'] = $expireday;
+                }
+                return $result;
+            }
+        } else {
+            return 2; // loi sql
+        }
+        
+    }
+    mysqli_close($connection); 
+}
+########tạo list user##############
+// $username = 'toancr';
+// $pass = 'toancr123';
+// $owner = 'toancr';
+// $count = 120;
+// $result = addlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username,$pass,$owner,$count);
+#########export list user###########
+//$username = 'sytt';
+//$result = exportlistuser ($sql_host,$sql_user,$sql_pass,$sql_db,$username);
+#########show list user###########
+// $owner = 'toancr';
+// $result = showuserall ($sql_host,$sql_user,$sql_pass,$sql_db,$owner);
+// print_r($result);
+
+##########update IP PUBLIC##############
+#$host = '172.31.1.1';
+#$result = showmik($sql_host,$sql_user,$sql_pass,$sql_db,$host, $mik_user, $mik_pass);
+#print_r($result);
+
 ?>
